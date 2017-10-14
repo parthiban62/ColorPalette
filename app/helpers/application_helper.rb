91be3fb7ecb62f06d7da1653a69f5d22fea0b853
parthiban62 <parthibanjayaraj@gmail.com>
2017-10-14 10:31:58 +0530

    code tweaks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,8 @@ module ApplicationHelper
    return Palette.find_color_code_and_user_stamp(i,j)
   end
 
-  def generate_tile(i,j,tile_info)
+  def generate_tile(i,j)
+   tile_info = get_tile_details(i,j)
    raw("<div onclick='toggleClass(this);' class='content' id='tile-#{i}_#{j}' style='background-color: #{tile_info[0]}' title='#{tile_info[1]}'></div>")
   end
 end
