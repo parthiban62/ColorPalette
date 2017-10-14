7f003b4023a811e8f9e735b88c2b62a26d858c51
parthiban62 <parthibanjayaraj@gmail.com>
2017-10-14 10:28:52 +0530

    Code alignment changes

--- app/models/palette.rb
+++ app/models/palette.rb
@@ -1,13 +1,11 @@
 class Palette < ApplicationRecord
-	belongs_to :user
-	validates_presence_of :code,:row,:column,:user_id
+  belongs_to :user
+  validates_presence_of :code,:row,:column,:user_id
 
-	def self.find_color_code_and_user_stamp(x,y)
-		result = []
-		palette = Palette.where(row: x,column: y).last
-		if palette.present?
-			result = [palette.code,"#{palette.user.username} #{palette.updated_at.strftime("%d-%m-%Y %H:%M:%S")}"]
-		end
-		return result
-	end
+  def self.find_color_code_and_user_stamp(x,y)
+   result = []
+   palette = Palette.where(row: x,column: y).last
+   result = [palette.code,"#{palette.user.username} #{palette.updated_at.strftime("%d-%m-%Y %H:%M:%S")}"] if palette.present?
+   return result
+  end
 end
