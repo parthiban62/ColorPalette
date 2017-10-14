e2ce3a62896348efb0e9f287cb1402ac942da996
parthiban62 <parthibanjayaraj@gmail.com>
2017-10-14 22:11:24 +0530

    alignment fixes

--- app/models/palette.rb
+++ app/models/palette.rb
@@ -3,13 +3,13 @@ class Palette < ApplicationRecord
   validates_presence_of :code,:row,:column,:user_id
 
   def self.find_color_code_and_user_stamp(x,y)
-   result = []
-   palette = Palette.where(row: x,column: y).last
-   result = [palette.code,"#{palette.user.username} #{palette.form_time_stamp}"] if palette.present?
-   result
+    result = []
+    palette = Palette.where(row: x,column: y).last
+    result = [palette.code,"#{palette.user.username} #{palette.form_time_stamp}"] if palette.present?
+    result
   end
 
   def form_time_stamp
-   self.updated_at.strftime("%d-%m-%Y %H:%M:%S")
+    self.updated_at.strftime("%d-%m-%Y %H:%M:%S")
   end
 end
