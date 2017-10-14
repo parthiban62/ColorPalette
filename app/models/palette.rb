2d18ecd6457e964f25a31bda6bfb4c2c06f23cf7
parthiban62 <parthibanjayaraj@gmail.com>
2017-10-14 22:03:31 +0530

    code tweaks

--- app/models/palette.rb
+++ app/models/palette.rb
@@ -5,7 +5,11 @@ class Palette < ApplicationRecord
   def self.find_color_code_and_user_stamp(x,y)
    result = []
    palette = Palette.where(row: x,column: y).last
-   result = [palette.code,"#{palette.user.username} #{palette.updated_at.strftime("%d-%m-%Y %H:%M:%S")}"] if palette.present?
-   return result
+   result = [palette.code,"#{palette.user.username} #{palette.form_time_stamp}"] if palette.present?
+   result
+  end
+
+  def form_time_stamp
+  	self.updated_at.strftime("%d-%m-%Y %H:%M:%S")
   end
 end
