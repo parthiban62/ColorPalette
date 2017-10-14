a5837a56ff4822e70ca7b35fa98c6f6f207f3c68
parthiban62 <parthibanjayaraj@gmail.com>
2017-10-14 10:05:25 +0530

    squares generation code moved to helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
     def get_tile_details(i,j)
       return Palette.find_color_code_and_user_stamp(i,j)
     end
+
+    def generate_tile(i,j,tile_info)
+    	raw("<div onclick='toggleClass(this);' class='content' id='tile-#{i}_#{j}' style='background-color: #{tile_info[0]}' title='#{tile_info[1]}'></div>")
+    end
 end
